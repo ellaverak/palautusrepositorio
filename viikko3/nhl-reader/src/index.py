0a5c3bb6e462d1789bdd02d283e46c999e01a874
ellaverak <ella.korkeaaho@gmail.com>
2022-11-18 13:57:05 +0200

    modify nhl-reader

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,30 @@
+import requests
+import datetime
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
+    response = requests.get(url).json()
 
-if __name__ == "__main__":
-    main()
+#    print("JSON-muotoinen vastaus:")
+#    print(response)
+
+    players = []
+
+    for player_dict in response:
+        if player_dict['nationality'] == "FIN":
+            player = Player(
+                player_dict['name'],
+                player_dict['team'],
+                player_dict['goals'],
+                player_dict['assists']
+            )
+
+            players.append(player)
+
+    print(f"Players from FIN {datetime.datetime.now()}:")
+
+    for player in players:
+        print(player)
+
+main()
