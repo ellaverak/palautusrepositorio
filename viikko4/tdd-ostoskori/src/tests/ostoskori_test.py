1f63493a19542c45da9cb3bb3514de99e0309c05
ellaverak <ella.korkeaaho@gmail.com>
2022-11-22 20:50:06 +0200

    tdd-ostoskori add: test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_on_kaksi_ostosta and corresponding functionality

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -68,3 +68,13 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_on_kaksi_ostosta(self):
+        maito = Tuote("Maito", 3)
+        makarooni = Tuote("Makarooni", 2)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(makarooni)
+
+        ostokset = self.kori.ostokset()
+
+        self.assertEqual(len(ostokset), 2)
