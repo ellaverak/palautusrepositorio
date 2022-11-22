7712b3a3262279556d4609dea0f5a94f9a376c3c
ellaverak <ella.korkeaaho@gmail.com>
2022-11-22 20:55:23 +0200

    tdd-ostoskori add: test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_on_yksi_ostos_jolla_sama_nimi_kuin_tuotteella_ja_lukumäärä_2 and corresponding functionality

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -87,3 +87,13 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
 
         self.assertEqual(len(ostokset), 1)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_on_yksi_ostos_jolla_sama_nimi_kuin_tuotteella_ja_lukumäärä_2(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        ostos = self.kori.ostokset()[0]
+
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+        self.assertEqual(ostos.lukumaara(), 2)
